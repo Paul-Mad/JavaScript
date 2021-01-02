60b352c9c7becedbc8c72b9e1d9fa650cedb8d20
Paul-Mad <paulin.mad@hotmail.com>
2021-01-02 20:26:49 +0000

    new game button implemented. init function

--- starter/script.js
+++ starter/script.js
@@ -11,10 +11,25 @@ const diceEl = document.querySelector('.dice');
 const btnnew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
-const score = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+let score, currentScore, activePlayer, playing;
+
+const init = function () {
+  score = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+};
+init();
 
 const swtichPlayer = function () {
   document.getElementById(
@@ -61,7 +76,7 @@ btnHold.addEventListener('click', function () {
     document.getElementById(`score--${activePlayer}`).textContent =
       score[activePlayer];
     // 2.Check if player's score is >= 100
-    if (score[activePlayer] >= 20) {
+    if (score[activePlayer] >= 100) {
       //  Finish the game
       playing = false;
       diceEl.classList.add('hidden');
@@ -78,3 +93,5 @@ btnHold.addEventListener('click', function () {
     }
   }
 });
+
+btnnew.addEventListener('click', init);
